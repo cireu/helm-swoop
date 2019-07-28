1b566775bc8c3436e3e3a5c244dd9f231af1d846
Zhu Zihao <all_but_last@163.com>
2019-07-28 17:00:32 +0800

    [fix] Missing `helm-swoop-pattern` and typo in `helm-multi-swoop--basic-map`.

--- helm-swoop.el
+++ helm-swoop.el
@@ -167,8 +167,8 @@
 (defvar helm-swoop-last-line-info nil)
 
 ;; Buffer local variables
-(defvar helm-swoop-pattern)            ; Keep helm-pattern value
-(defvar helm-swoop-last-query)         ; Last search query for resume
+(defvar helm-swoop-pattern nil)            ; Keep helm-pattern value
+(defvar helm-swoop-last-query nil)         ; Last search query for resume
 (defvar-local helm-swoop-last-prefix-number 1) ; For multiline highlight
 
 ;; Global variables
